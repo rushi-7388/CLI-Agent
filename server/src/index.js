e213b571255d99dd6dffcfe66e2beeac8684bc78
Rushi Dave <rushidave7388@gmail.com>
2025-12-16 21:34:48 +0530

    First Initial commit

---
+++ server/src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.get('/health', (req, res) => {
+  res.send('OK');
+})
+
+app.listen(process.env.PORT , () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+})
